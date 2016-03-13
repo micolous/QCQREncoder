a194501c3f6157338d3891ab6a62a3b0c8f2d144
Michael Farrell <micolous+git@gmail.com>
2016-03-13 21:32:00 +1100

    Convert to Objective-C ARC (fixes #1)
    Add "Build & Copy" scheme as shared scheme

--- QCQREncoderPlugIn.m
+++ QCQREncoderPlugIn.m
@@ -106,24 +106,6 @@ Here you need to declare the input / output properties as dynamic as Quartz Comp
 	return self;
 }
 
-- (void) finalize
-{
-	/*
-	Release any non garbage collected resources created in -init.
-	*/
-	
-	[super finalize];
-}
-
-- (void) dealloc
-{
-	/*
-	Release any resources created in -init.
-	*/
-	
-	[super dealloc];
-}
-
 @end
 
 @implementation QCQREncoderPlugIn (Execution)
